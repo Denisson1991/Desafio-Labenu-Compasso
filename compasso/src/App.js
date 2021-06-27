7a8df6421018a081d9751a89d22bca5febc82468
Denisson-Silva <denisson.boulevardmotos@gmail.com>
2021-06-27 15:10:17 -0300

    Versão para entrega

--- compasso/src/App.js
+++ compasso/src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import CardPerfil from "./pages/CardPerfil";
 import Starred from "./pages/Starred";
 import Home from "./pages/Home";
+import Perfil from "./pages/Perfil";
 import Repos from "./pages/Repos";
-import styled from 'styled-components'
 
-const Header = styled.div`
-border: 1px solid black;
-padding: 10px;
-text-align: center;
 
-`
+
 
 
  function App() {
@@ -23,18 +18,22 @@ text-align: center;
           <Switch>
             <Route exact path="/">
                 <Home/>
+            </Route>   
+
+            <Route exact path="/perfil/:name">
+                <Perfil/>
             </Route>            
 
             <Route exact path="/repositorios/:name">
                 <Repos/>
             </Route>
 
-            <Route exact path="/starred/:nome">
+            <Route exact path="/starred/:name">
                 <Starred/>
             </Route>
 
             <Route>
-                <div>deu erro</div>
+                <div><iframe src="https://giphy.com/embed/8L0Pky6C83SzkzU55a" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
             </Route>
 
 
--- compasso/src/pages/Home.js
+++ compasso/src/pages/Home.js
@@ -1,77 +1,95 @@
 import React from 'react'
-import axios from 'axios'
-import CardPerfil from "./CardPerfil"
 import { useEffect, useState } from "react";
 import styled from 'styled-components'
+import InputBase from '@material-ui/core/InputBase';
+import Search from '@material-ui/icons/Search';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import { useHistory } from 'react-router-dom';
+import ForwardIcon from '@material-ui/icons/Forward';
+import Button from '@material-ui/core/Button';
+
+
 
 const Header = styled.div`
 border: 1px solid black;
 padding: 10px;
 text-align: center;
+background: #2C5364;
+`
+const InputBorder = styled.div`
+border: 1px solid black;
+width: 200px;
+border-radius: 5px;
+margin-right: 5px;
+`
+const Busca = styled.div`
+display: flex;
+justify-content: center;
+`
+const Raiz = styled.div`
+background: #203A43;
+margin: 0px;
+padding: 0px;
+height: 97vh;
+width: 100vw;
+color: white;
+font-family: Roboto;
+`
+const ImagemGit = styled.div`
+border: 1px solid black;
+text-align: center;
+height: 450px;
+width: 300px;
+margin-left: 560px;
+margin-top: 30px;
+padding: 8px;
+display: inline-block;
+position: relative;
+background: #2C5364;
 `
 
-export default function Home () {
-  const [inputNomeUsuario, setInputNomeUsuario] = useState("")
-  const [arrayUsuarios, setArrayUsuarios] = useState([])
-  const [valorCard, setValorCard] = useState("nocard")
 
+export default function Home () {
+  const history = useHistory()
+  const [inputNomeUsuario, setInputNomeUsuario] = useState("")
+  
   const inputUsuario = (event) => {
     setInputNomeUsuario(event.target.value)
   }
 
-  const buscaUsuario = () => {       
-    const url = `https://api.github.com/users/${inputNomeUsuario}`
-      axios.get(url, 
-          {headers: {
-                Authorization: "6af07f86033707a4e1e4"
-      }})
-    .then((response) => {  
-      setArrayUsuarios(response.data)
-      setInputNomeUsuario("")
-      setValorCard("card")
-      
-      })
 
-    .catch((error) => {
-      console.log(error.response);
-      });
-     
-}
 
-const renderizaPag = () => {
-  switch (valorCard) {
-    case "card":
-      return <CardPerfil
-      input={inputNomeUsuario}
-      setArray={setArrayUsuarios}
-      arrayUsuarios={arrayUsuarios}
-      setInput={setInputNomeUsuario}/>
-    case "nocard":
-      return <p>sem card</p>;
-    default:
-      return <div></div>;
-  }
-};
+    const goToPerfil = (name) => {
+      history.push(`/perfil/${name}`)
+    }
     
   
   return (  
-    <div>    
-     <Header>
-       
-       <h1>Compasso Github</h1>
-      <input placeholder="GitUser" onChange={inputUsuario} value={inputNomeUsuario}></input>
-      <button onClick={buscaUsuario}>Buscar Usuário</button>
+    <Raiz>   
 
+     <Header>       
+      <h1>Compasso Github</h1>
+      <Busca>
+        <InputBorder>
+          <InputBase
+          placeholder={'GitUsuário'}
+          startAdornment={<Search />}
+          onChange={inputUsuario} 
+          value={inputNomeUsuario}
+          />
+        </InputBorder>
+        <Button variant="contained" color="default" component="span" onClick={() => goToPerfil(inputNomeUsuario)} >Buscar<ForwardIcon/></Button>
+      </Busca>
      </Header> 
 
-    <div>{renderizaPag()}</div>
+     <ImagemGit >
+        <ArrowUpwardIcon size="large"/>
+        <h2>Faça aqui a sua pesquisa</h2>
+        <iframe src="https://giphy.com/embed/du3J3cXyzhj75IOgvA" ></iframe>
+      </ImagemGit>
+
     
 
-     
-        
-      
-      
-
-    </div>
+    </Raiz>
   );
 }
